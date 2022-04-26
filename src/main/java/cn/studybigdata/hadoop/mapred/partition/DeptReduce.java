88ba04464fc9c4fe1aef2cde5ea54bd1f20ef405
A-stranger <qingyuan.dev@Live.com>
2022-04-26 11:11:51 +0800

    1. partition 2. combine

---
+++ src/main/java/cn/studybigdata/hadoop/mapred/partition/DeptReduce.java
@@ -0,0 +1,18 @@
+package cn.studybigdata.hadoop.mapred.partition;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class DeptReduce extends Reducer<IntWritable, Employee, IntWritable, Text> {
+    @Override
+    protected void reduce(IntWritable dept, Iterable<Employee> employees, Context context) throws IOException, InterruptedException {
+
+        for (Employee employee : employees) {
+            context.write(dept, new Text(employee.toString()));
+        }
+    }
+}
